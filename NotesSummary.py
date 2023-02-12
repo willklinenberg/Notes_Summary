453f0c6b7c70be031be33aa549a712d91a0f8e03
Will Klinenberg <111905184+willklinenberg@users.noreply.github.com>
2023-02-12 14:34:28 -0500

    Update NotesSummary.py

--- NotesSummary.py
+++ NotesSummary.py
@@ -4,22 +4,20 @@ import streamlit as st
 import promptlayer
 promptlayer.api_key = "pl_24c0d07de627be146bd5327daa373694"
 
-import docx2txt
 
-path = st.text_input(label = "Specify the path to your notes")
+text = st.file_uploader("Choose a Notes file")
 # extract text
-if path:
-    text = docx2txt.process(path)
+if text:
 
 
     # Swap out your 'import openai'
     openai = promptlayer.openai
-    openai.api_key = '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'
+    openai.api_key = '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'
 
     # Do something fun 🚀
     summary = openai.Completion.create(
     engine="text-davinci-003", 
-    prompt=text + '\n Please write a brief summary based on the notes above', 
+    prompt=str(text) + '\n Please write a brief summary based on the notes above', 
     pl_tags=["name-guessing", "pipeline-2"],
     max_tokens=500
     )
